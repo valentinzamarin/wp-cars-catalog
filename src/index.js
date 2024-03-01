5252bf36d24f7d2651adce98093f5570aca4c33d
valentinzamarin <hello.zamarin@gmail.com>
2024-03-01 16:12:36 +0400

    reset button

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 import filterFormData from "./utilitis/formdata";
 class CarsCatalog {
-
-
     constructor() {
         this.filterForm = document.querySelector('#filter');
 
         if ( this.filterForm  == null) {
             return false
         }
+
+        this.filterResponseBlock = document.querySelector('#filter-response');
 
         this.filterForm.addEventListener('submit', event => {
             this.filterHandler(event)
@@ -19,6 +19,13 @@ class CarsCatalog {
                 this.paginationHandler( event );
             })
         })
+
+        document.querySelector('#btn-reset').addEventListener( 'click', event => {
+            this.resetFormHandler( event )
+        })
+
+        this.postObserver();
+
     }
 
     async filterHandler(event) {
@@ -31,11 +38,6 @@ class CarsCatalog {
 
         document.querySelector('#filter-response').innerHTML = response.data.data.posts;
 
-        document.querySelectorAll( '.page-link' ).forEach( link => {
-            link.addEventListener( 'click', event => {
-                this.paginationHandler( event );
-            })
-        })
     }
 
     async paginationHandler(event) {
@@ -51,11 +53,39 @@ class CarsCatalog {
 
         document.querySelector('#filter-response').innerHTML = response.data.data.posts;
 
-        document.querySelectorAll( '.page-link' ).forEach( link => {
-            link.addEventListener( 'click', event => {
-                this.paginationHandler( event );
-            })
-        })
+    }
+
+    async resetFormHandler( event ) {
+        event.preventDefault();
+
+        this.filterForm.reset();
+        let filterData = filterFormData( this.filterForm , 1 );
+
+        const admin_ajax_url = theme_ajax.ajax_url;
+        const response = await axios.post(admin_ajax_url, filterData );
+
+        document.querySelector('#filter-response').innerHTML = response.data.data.posts;
+
+
+    }
+
+    postObserver() {
+        this.observer = new MutationObserver((mutationsList, observer) => {
+            for(let mutation of mutationsList) {
+                if (mutation.type === 'childList') {
+
+                    document.querySelectorAll( '.page-link' ).forEach( link => {
+                        link.addEventListener( 'click', event => {
+                            this.paginationHandler( event );
+                        })
+                    })
+
+                }
+            }
+        });
+
+        const config = { childList: true, subtree: true };
+        this.observer.observe( this.filterResponseBlock , config);
     }
 }
 
